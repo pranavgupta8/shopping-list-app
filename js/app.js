4117230b7d6f088ae68001eccf29a02b2b554734
Pranav Gupta <pranavgupta8@hotmail.com>
2016-08-21 21:27:26 +0530

    minor bug fixes

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 	var i=1;
 	var newitem=null;
 
-	$('p')
+	$('#add')
 	.css('cursor', 'pointer')
 
 	.mouseover(function(){
@@ -21,10 +21,17 @@ $( document ).ready(function() {
 		
 		else {
 			$('.new-items').fadeOut("fast");
-			click=1;}
-		});
+			click =	1;}
+	});
 
-	$('#add').click(function(e) { //New Item Addition
+	$('#empty')
+	.css('cursor', 'pointer')
+
+	.click(function(){
+		$('.dicarded ul').html("");
+	});
+
+	$('#submit').click(function(e) { //New Item Addition
 	
 		e.preventDefault();
 		
@@ -51,18 +58,22 @@ $( document ).ready(function() {
 		if (event.keyCode == 13) {
 		
 			if (newitem != null || newitem != "") {
-				$('#add').click();
+			
+				$('#submit').click();
 				return false;
 			}
 		
 			else {
+			
 				 alert('Please put in some Item Name');
 				 return false;
 			}
 		}
 	});
 	
-	$('.on-list ul').on('click', 'li', function(){ //Item moved from List to Cart
+	$('.on-list ul')
+
+	.on('click', 'li', function(){ //Item moved from List to Cart
 		
 		var cartitem = $( this ).text();
 		$('.in-cart ul').append($('<li><input type="checkbox" checked name="item' + i + '" value="' + cartitem + '"/> ' + cartitem + '</li>')).hide().fadeIn("medium");
@@ -70,9 +81,16 @@ $( document ).ready(function() {
 		
 		if(i>1) i--;
 
+	})
+
+	.mouseenter(function(){
+
+		$('li').append();
 	});
 	
-	$('.in-cart ul').on('click', 'li', function(){ //Item moved from Cart to List
+	$('.in-cart ul')
+
+	.on('click', 'li', function(){ //Item moved from Cart to List
 		
 		var listitem = $( this ).text();
 		$('.on-list ul').append($('<li><input type="checkbox" name="item' + i + '" value="' + listitem + '"/> ' + listitem + '</li>')).hide().fadeIn("medium");
